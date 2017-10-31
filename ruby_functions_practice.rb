af1baa00cc97b572549378e6898bacda0ec00853
Malcolm <malcolmfinlayson81@gmail.com>
2017-10-31 17:45:36 +0000

    Tests completed and all functions working

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -18,27 +18,83 @@ def divide(num1, num2)
   return (num1 / num2).round(2)
 end
 
-# def length_of_string(string)
-#   return string.length
-# end
-#
-# def join_string()
-# end
-#
-# def add_string_as_number
-# end
-#
-# def number_to_full_name
-# end
-#
-# def substring
-# end
-#
-# def volume_of_cube
-# end
-#
-# def volume_of_sphere
-# end
-#
-# def fahrenheit_to_celsius
-# end
+
+
+def length_of_string(string)
+   return string.length
+end
+
+def join_string(string_1, string_2)
+  return string_1 + string_2
+end
+
+def add_string_as_number(num_string_1, num_string_2)
+  return num_string_1.to_f + num_string_2.to_f
+end
+
+def number_to_full_month_name(month_number)
+  #return Date::MONTHNAMES[month_number]
+  case month_number
+  when 1
+    return 'January'
+  when 2
+    return 'February'
+  when 3
+    return 'March'
+  when 4
+    return 'April'
+  when 5
+    return 'May'
+  when 6
+    return 'June'
+  when 7
+    return 'July'
+  when 8
+    return 'August'
+  when 9
+    return 'September'
+  when 10
+    return 'October'
+  when 11
+    return 'November'
+  when 12
+    return 'December'
+  else
+    return 'Please enter a number between 1 and 12'
+  end
+end
+
+def number_to_short_month_name(month_number)
+  #return Date::ABBR_MONTHNAMES[month_number]
+  monthHash = {
+    1 => 'Jan',
+    2 => 'Feb',
+    3 => 'Mar',
+    4 => 'Apr',
+    5 => 'May',
+    6 => 'Jun',
+    7 => 'Jul',
+    8 => 'Aug',
+    9 => 'Sep',
+    10 => 'Oct',
+    11 => 'Nov',
+    12 => 'Dec'
+  }
+  return monthHash[month_number]
+end
+
+
+def volume_of_cube(num)
+  return num ** 3
+end
+
+def volume_of_sphere(num)
+  pi = 3.141592653
+  four_by_three = 4.to_f / 3
+  return (four_by_three * pi * (num**3)).round(2)
+end
+
+def fahrenheit_to_celsius(fahrenheit)
+  celcius = (fahrenheit - 32) * (5/9.to_f)
+  return celcius.round(2)
+end
--- specs/ruby_functions_spec.rb
+++ specs/ruby_functions_spec.rb
@@ -28,60 +28,84 @@ class Functions_Practice < MiniTest::Test
     assert_equal( 5, divide_result )
   end
 
-#   def test_length_of_string()
-#     test_string = "A string of length 21"
-#     length_of_string = length_of_string( test_string )
-#     assert_equal( 21, length_of_string )
-#   end
-#
-#   def test_join_string()
-#     string_1 = "Mary had a little lamb, "
-#     string_2 = "it's fleece was white as snow"
-#     joined_string = join_string( string_1, string_2 )
-#     assert_equal( "Mary had a little lamb, it's fleece was white as snow", joined_string )
-#   end
-#
-#   def test_add_string_as_number()
-#     add_result = add_string_as_number( "1", "2" )
-#     assert_equal( 3, add_result )
-#   end
-#
-#   def test_number_to_full_name()
-#     first_month_string = number_to_full_month_name( 1 )
-#     third_month_string = number_to_full_month_name( 3 )
-#     ninth_month_string = number_to_full_month_name( 9 )
-#     assert_equal( "January", first_month_string )
-#     assert_equal( "March", third_month_string )
-#     assert_equal( "September", ninth_month_string )
-#   end
-#
-#   def test_substring()
-#     first_month_string = number_to_short_month_name( 1 )
-#     third_month_string = number_to_short_month_name( 3 )
-#     ninth_month_string = number_to_short_month_name( 9 )
-#     assert_equal( "Jan", first_month_string )
-#     assert_equal( "Mar", third_month_string )
-#     assert_equal( "Sep", ninth_month_string )
-#   end
-#
-#
-#
-#   #Further
-#
-#   #Given the length of a side of a cube calculate the volume
-#   def test_volume_of_cube()
-#     #add test code here
-#   end
-#
-#   #Given the radius of a sphere calculate the volume
-#   def test_volume_of_sphere()
-#     #add test code here
-#   end
-#
-#   #Given a value in farenheit, convert this into celsius.
-#   def test_fahrenheit_to_celsius()
-#     #add test code here
-#   end
-#
-#
+  def test_length_of_string()
+    test_string = "A string of length 21"
+    length_of_string = length_of_string( test_string )
+    assert_equal( 21, length_of_string )
+  end
+
+  def test_join_string()
+    string_1 = "Mary had a little lamb, "
+    string_2 = "it's fleece was white as snow"
+    joined_string = join_string( string_1, string_2 )
+    assert_equal( "Mary had a little lamb, it's fleece was white as snow", joined_string )
+  end
+
+  def test_add_string_as_number()
+    add_result = add_string_as_number( "1", "2" )
+    assert_equal( 3, add_result )
+  end
+
+  def test_number_to_full_name()
+    first_month_string = number_to_full_month_name( 1 )
+    third_month_string = number_to_full_month_name( 3 )
+    ninth_month_string = number_to_full_month_name( 9 )
+    assert_equal( "January", first_month_string )
+    assert_equal( "March", third_month_string )
+    assert_equal( "September", ninth_month_string )
+  end
+
+  def test_substring()
+    first_month_string = number_to_short_month_name( 1 )
+    third_month_string = number_to_short_month_name( 3 )
+    ninth_month_string = number_to_short_month_name( 9 )
+    assert_equal( "Jan", first_month_string )
+    assert_equal( "Mar", third_month_string )
+    assert_equal( "Sep", ninth_month_string )
+  end
+
+#  Further
+
+#  Given the length of a side of a cube calculate the volume
+
+  def test_volume_of_cube()
+   #ARRANGE
+   #ACT
+   three_by_three_cube = volume_of_cube( 3 )
+   five_by_five_cube = volume_of_cube( 5 )
+   ten_by_ten_cube = volume_of_cube( 10 )
+   #ASSERT
+   assert_equal( 27 , three_by_three_cube )
+   assert_equal( 125 , five_by_five_cube )
+   assert_equal( 1000 , ten_by_ten_cube )
+  end
+
+#  Given the radius of a sphere calculate the volume
+
+  def test_volume_of_sphere()
+     #ARRANGE
+     #ACT
+     sphere_radius_7 = volume_of_sphere( 7 )
+     sphere_radius_13 = volume_of_sphere( 13 )
+     sphere_radius_1 = volume_of_sphere ( 1 )
+     #ASSERT
+     assert_equal( 1436.76 , sphere_radius_7 )
+     assert_equal( 9202.77 , sphere_radius_13 )
+     assert_equal( 4.19 , sphere_radius_1 )
+  end
+
+# Given a value in farenheit, convert this into celsius.
+
+  def test_fahrenheit_to_celsius()
+     #ARRANGE
+     #ACT
+     fahrenheit_of_90 = fahrenheit_to_celsius( 90 )
+     fahrenheit_of_15 = fahrenheit_to_celsius( 15 )
+     fahrenheit_of_35 = fahrenheit_to_celsius( 35 )
+     #ASSERT
+     assert_equal( 32.22 , fahrenheit_of_90 )
+     assert_equal( -9.44 , fahrenheit_of_15 )
+     assert_equal( 1.67 , fahrenheit_of_35 )
+  end
+
 end
