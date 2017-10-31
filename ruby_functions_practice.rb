5a76d6f458aba95d1d8f337ba7077439a71e4b79
Malcolm <malcolmfinlayson81@gmail.com>
2017-10-31 16:05:44 +0000

    return_10 test passed

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,39 @@
+def return_10
+  return 10
+end
+
+def add(num1, num2)
+end
+
+def subtract(num1, num2)
+end
+
+def multiply(num1, num2)
+end
+
+def divide(num1, num2)
+end
+
+def length_of_string(string)
+end
+
+def join_string
+end
+
+def add_string_as_number
+end
+
+def number_to_full_name
+end
+
+def substring
+end
+
+def volume_of_cube
+end
+
+def volume_of_sphere
+end
+
+def fahrenheit_to_celsius
+end
